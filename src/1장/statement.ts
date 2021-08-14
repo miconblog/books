523ccdd5625f45abe7b1a991e5823d3fee2ae196
불꽃남자 <miconblog@gmail.com>
2021-08-14 22:37:47 +0900

    값이 변경되지 않은 캐시형태의 로컬 변수 제거

--- src/1장/statement.ts
+++ src/1장/statement.ts
@@ -52,8 +52,6 @@ export function statement(invoice: Invoice, plays: Plays): string {
     .format;
 
   for (let perf of invoice.performance) {
-    let thisAmount = amountFor(perf);
-
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -61,8 +59,8 @@ export function statement(invoice: Invoice, plays: Plays): string {
     if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+    totalAmount += amountFor(perf);
   }
 
   result += `총액: ${format(totalAmount / 100)}\n`;
